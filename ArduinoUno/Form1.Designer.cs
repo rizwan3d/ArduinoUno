91504827b42d7fecf012365f6dd600ee42f2a443
Muhammad Rizwan <allgame45@hotmail.com>
2019-04-30 18:46:42 +0500

    dsdf

--- ArduinoUno/Form1.Designer.cs
+++ ArduinoUno/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(32, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(32, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 47);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(200, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 47);
+            this.button3.TabIndex = 0;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "90";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LED";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 33);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Motor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(308, 264);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ArduinoUno/Form1.cs
+++ ArduinoUno/Form1.cs
@@ -6,14 +6,68 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.IO.Ports;
 namespace ArduinoUno
 {
     public partial class Form1 : Form
     {
+
+        SerialPort port;
+
+        int a = 90;
+
         public Form1()
         {
             InitializeComponent();
+
+            port = new SerialPort("COM5", 9600);
+            port.Open();
+
+            this.FormClosed += Form1_FormClosed;
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            port.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (b.Text.Equals("On"))
+            {
+                b.Text = "Off";
+                port.Write("200");
+            }
+            else
+            {
+                b.Text = "On";
+                port.Write("250");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a += 10;
+            if (a > 180)
+                a = 180;
+            port.Write(a.ToString());
+            label1.Text = a.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a -= 10;
+            if (a < 0)
+                a = 0;
+            port.Write(a.ToString());
+            label1.Text = a.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
